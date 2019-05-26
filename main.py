22a86025772afbba6a1ad91cfe6c24e57e117633
Azubi <krankenreif@gmail.com>
2019-05-26 12:05:25 +0200

    Add conversation handler

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
+import logging
 
 # local imports
 from skybot.telegram_bot import TelegramBot
 
 def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
     cmd_arguments = sys.argv[1:]
     if len(cmd_arguments) >= 2:
         if cmd_arguments[0] == "-telegram":
--- skybot/telegram_bot.py
+++ skybot/telegram_bot.py
@@ -1,21 +1,60 @@
 from skybot.api import API
 import requests
 from threading import Thread
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
+from telegram.ext import CommandHandler, CallbackQueryHandler, Updater, ConversationHandler, MessageHandler, Filters
+from telegram import KeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyboardButton
 
 class TelegramBot():
+
+    CLUB, PILOT = range(2)
 
     def __init__(self, bot_token):
         self.__api = API(requests)
         updater = Updater(token=bot_token)
         dispatcher = updater.dispatcher 
-        start_handler = CommandHandler('live', self.send_live)
-        dispatcher.add_handler(start_handler)
+        live_handler = CommandHandler(command='live', callback=self.send_live)
+        add_handler = CommandHandler(command='add', callback=self.add)    
+        dispatcher.add_handler(CallbackQueryHandler(self.add_pilot, pattern='add_pilot'))
+
+        club_add_handler = ConversationHandler( entry_points=[CallbackQueryHandler(callback=self.add_club, pattern='add_club')],
+                                                states={TelegramBot.CLUB: [MessageHandler(filters=Filters.regex("^\d+$"), callback=self.club_answer)]},
+                                                fallbacks=[CommandHandler(command='cancel', callback=self.cancel)])
+        dispatcher.add_handler(live_handler)
+        dispatcher.add_handler(add_handler)
+        dispatcher.add_handler(club_add_handler)
         updater.start_polling()
+        updater.idle()
 
     def send_live(self, bot, update):
         print("send live")
         bot.send_message(chat_id=update.message.chat_id, text=self.__api.get_live())
+
+    def add(self, bot, update):
+        print("add")
+        keyboard = [[InlineKeyboardButton(text='pilot', callback_data="add_pilot")],
+            [InlineKeyboardButton(text='club', callback_data="add_club")]]
+        bot.send_message(chat_id=update.message.chat_id, text="Select what to add", reply_markup=InlineKeyboardMarkup(keyboard))
+        
+    def add_club(self, bot, update):
+        print("add club")
+        query = update.callback_query
+        club_ask = bot.edit_message_text(chat_id=query.message.chat_id, message_id=query.message.message_id, text="Please enter club number (only numbers allowed)")
+        return TelegramBot.CLUB
+
+    def club_answer(self, bot, update):
+        update.message.reply_text(f"Club ")
+        update.message.reply_text("thank you")
+        return ConversationHandler.END
+
+    def add_pilot(self, bot, update):
+        print("add pilot")
+
+    def add_keyboard():
+        keyboard = [[KeyboardButton('pilot', callback_data='add_pilot')],
+            [KeyboardButton('club', callback_data='add_club')]]
+        return ReplyKeyboardMarkup(keyboard)
+
+    def cancel(update, context):
+        print(update.message.from_user)
+        update.message.reply_text('Bye! I hope we can talk again some day.')
+        return ConversationHandler.END
